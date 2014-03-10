56cf0f30650adeb3e430631c7951caec1d604667
David Whitney <dwhitnee@gmail.com>
2014-03-09 21:44:47 -0700

    Fix the crap yahoo did to the 2014 my.yahoo.com layouts

---
+++ greasemonkey/yahoo2col.js
@@ -0,0 +1,14 @@
+//----------------------------------------
+// Fix the crap yahoo did to the 2014 my.yahoo.com layouts
+// Remove ad, and change to 2 column 33/66
+//
+// 2014, David Whitney
+//---------------------------------------- 
+
+var ad = document.getElementById("my-adsLREC");
+ad.style.display = "none";
+
+var columns = document.getElementsByClassName("myColumn");
+
+columns[0].style.width="33%";
+columns[1].style.width="100%";
