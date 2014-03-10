cd61d956b62e67578e76bda3eee6814f44506fca
David Whitney <dwhitnee@gmail.com>
2014-03-09 21:47:11 -0700

    Add greasemonkey goo

--- greasemonkey/yahoo2col.user.js
+++ greasemonkey/yahoo2col.user.js
@@ -1,3 +1,10 @@
+// ==UserScript==
+// @name          Fix My Yahoo Column widths 33/66gi
+// @namespace     http://davidwhitney.net/projects/greasemonkey/
+// @description   tv.com add episode list on main page
+// @include       http://my.yahoo.com/*
+// ==/UserScript==
+
 //----------------------------------------
 // Fix the crap yahoo did to the 2014 my.yahoo.com layouts
 // Remove ad, and change to 2 column 33/66
