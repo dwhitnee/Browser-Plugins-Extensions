0372ae3263a1ec8395b040f08f84bb5f4fe1ddc3
David Whitney <dwhitnee@gmail.com>
2016-04-13 20:13:46 -0700

    crosswords greasemonkey

---
+++ greasemonkey/nyt.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name          NYT Crosswords without ads
+// @namespace     http://davidwhitney.net/projects/greasemonkey/
+// @description   Remove the ads from NYT crossword page. We already paid $40 for the damsn thing
+// @include       http://www.nytimes.com/*
+// ==/UserScript==
+
+(function() {
+
+   var ads = document.getElementsByClassName("ad");
+
+   for (var i=0; i < ads.length; i++) {
+     ads[i].style.setProperty("display", "none", "important");
+   }
+
+   console.log("NYT ads");
+})();
