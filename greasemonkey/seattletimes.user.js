dff607bd62d4a702e2704c4196fa36119be51b14
David Whitney <dwhitnee@gmail.com>
2017-01-06 11:10:34 -0800

    ST

---
+++ greasemonkey/seattletimes.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name          Seattle Times print edition cleanup
+// @namespace     http://davidwhitney.net/projects/greasemonkey/
+// @description   Fix Seattle Times layout to my liking
+// @include       http://digital.olivesoftware.com/Olive/ODN/SeattleTimes/*
+// ==/UserScript==
+
+(function() {
+
+   function addGlobalStyle(css) {
+     var head, style;
+     head = document.getElementsByTagName('head')[0];
+     if (!head) { return; }
+     style = document.createElement('style');
+     style.type = 'text/css';
+     style.innerHTML = css;
+     head.appendChild(style);
+   }
+
+   function doOnLoad() {
+     addGlobalStyle('#toobar { top: 0; }');
+     addGlobalStyle('restHeightContainer { top: 4em; z-index: 1;}');
+   }
+
+   doOnLoad();
+
+})();
