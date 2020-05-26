7c999a0ab933edfcf44fd64b31f4c7fc82c9e491
David Whitney <dwhitnee@gmail.com>
2020-05-25 21:55:06 -0700

    timestamp

--- greasemonkey/cookieClicker.user.js
+++ greasemonkey/cookieClicker.user.js
@@ -1,44 +1,59 @@
 // ==UserScript==
 // @name          Find cookies
 // @namespace     http://davidwhitney.net/projects/greasemonkey/
-// @description   Find cookies
+// @description   So much cheating
 // @include       https://orteil.dashnet.org/*
 // ==/UserScript==
 
 (function() {
 
+   //------------------------------
+   // click magic cookies when they appear
+   //------------------------------
    function checkForCookies() {
      let cookieWrapper = document.getElementById("shimmers");
      
      if (cookieWrapper && cookieWrapper.children.length) {
        console.log("COOKIE!");
-       cookieWrapper.style.height = "30em";   // sorta cheating
-       cookieWrapper.style.width = "30em";
-       cookieWrapper.style.backgroundColor = "yellow";
+       timeStamp();
+       // cookieWrapper.style.height = "30em";   // sorta cheating
+       // cookieWrapper.style.width = "30em";
+       // cookieWrapper.style.backgroundColor = "yellow";
        cookieWrapper.children[0].click();   // super cheating
      } else {
        // restore?
-       cookieWrapper.style.height = "";
-       cookieWrapper.style.width = "";
-       cookieWrapper.style.backgroundColor = "";
+       // cookieWrapper.style.height = "";
+       // cookieWrapper.style.width = "";
+       // cookieWrapper.style.backgroundColor = "";
      }
    }
 
+   //------------------------------
    // cast conjure baked goods when magic is at max
+   //------------------------------
    function checkGrimoire() {
      let t = document.getElementById("grimoireBarText");
      let state = t.innerText.split("/");
-     console.log( state[0] + " magic available");
-     if (state[0] == state[1]) {       // max charge, lets cast a spell!
+     // console.log( state[0] + " magic available");
+     if (state[0] == state[1]) {       // max charge
        console.log("CASTING SPELL!");
+       timeStamp();
        let conjureBakedGoods = document.getElementById("grimoireSpell0");
        conjureBakedGoods.click();
     }
    }
 
+   function timeStamp() {
+     let c = new Date();
+     console.log( c.toLocaleTimeString() );
+   }
+
+   //----------------------------------------
+   // set up auto checks
+   //----------------------------------------
    function doOnLoad() {
      setInterval( function() { checkForCookies(); }, 3 * 1000);
-     setInterval( function() { checkGrimoire(); }, 1 * 60 * 1000);
+     setInterval( function() { checkGrimoire(); }, 60 * 1000);
    };
 
    doOnLoad();
