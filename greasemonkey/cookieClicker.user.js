0a8fbf48bb80b78df30ba9846c9fa79e356cc57c
David Whitney <dwhitnee@gmail.com>
2020-05-26 16:55:06 -0700

    inactivity check

--- greasemonkey/cookieClicker.user.js
+++ greasemonkey/cookieClicker.user.js
@@ -7,6 +7,20 @@
 
 (function() {
 
+   let spellWasCast = false;
+
+   //----------------------------------------
+   // if nothing happens for 30 minutes cast a spell anyway
+   //----------------------------------------
+   function inactivityCheck() {
+     if (!spellWasCast) {
+       console.log("No spells in 30 minutes");
+       castConjureBakedGoods();
+     }
+     spellWasCast = false;
+     setTimer( inactivityCheck, 30 * 60 * 1000 );
+   }
+
    //------------------------------
    // click magic cookies when they appear
    //------------------------------
@@ -16,17 +30,19 @@
      if (cookieWrapper && cookieWrapper.children.length) {
        console.log("COOKIE!");
        timeStamp();
-       // cookieWrapper.style.height = "30em";   // sorta cheating
-       // cookieWrapper.style.width = "30em";
-       // cookieWrapper.style.backgroundColor = "yellow";
-       cookieWrapper.children[0].click();   // super cheating
-       spamCookie( 100 );
-     } else {
-       // restore?
-       // cookieWrapper.style.height = "";
-       // cookieWrapper.style.width = "";
-       // cookieWrapper.style.backgroundColor = "";
+       for (var i=0; i< cookieWrapper.children.length; i++) {
+         cookieWrapper.children[i].click();   // super cheating
+       }
+       spamCookie( 1000 );
      }
+   }
+
+   // raise a big yellow flag
+   function raiseAlert() {
+     let cookieWrapper = document.getElementById("shimmers");
+     cookieWrapper.style.height = "30em";
+     cookieWrapper.style.width = "30em";
+     cookieWrapper.style.backgroundColor = "yellow";
    }
 
    //----------------------------------------
@@ -34,6 +50,8 @@
    //----------------------------------------
    function spamCookie( times ) {
      let bigCookie = document.getElementById("bigCookie");
+
+     // click every 10ms
      let spammer = setInterval( 
        function() { 
          bigCookie.click(); 
@@ -43,21 +61,38 @@
        }, 10);
    }
 
+
+   //-----------------------------------------
+   function castConjureBakedGoods() {
+     console.log("CASTING SPELL!"); timeStamp();
+     let conjureBakedGoods = document.getElementById("grimoireSpell0");
+     conjureBakedGoods.click();
+     spellWasCast = true;
+   }
+   
    //------------------------------
-   // cast conjure baked goods when magic is at max
+   // cast Conjure Baked Goods when magic is at max
+   // Format of magic: "33/36 (+0.03/s)"
+   // This fails if the grimoire is closed or window sleeps so this element
+   // is no longer updated.
+   // Cast a spell every 15 minutes instead? 
+   // How to check for sleep? If no spell for 30 min, auto cast?
+   // 
+   // At highter levels Force the Hand of Fate when a cookie shows up is
+   // probably better
    //------------------------------
    function checkGrimoire() {
      let t = document.getElementById("grimoireBarText");
-     let state = t.innerText.split("/");
-     // console.log( state[0] + " magic available");
-     if (state[0] == state[1]) {       // max charge
-       console.log("CASTING SPELL!");
-       timeStamp();
-       let conjureBakedGoods = document.getElementById("grimoireSpell0");
-       conjureBakedGoods.click();
-    }
+
+     // Format of magic: "33/36 (+0.03/s)"
+     let manna = t.innerText.split(" ")[0].split("/");
+
+     if (manna[0] == manna[1]) {       // max charge
+       castConjureBakedGoods();
+     }
    }
 
+   //----------------------------------------
    function timeStamp() {
      let c = new Date();
      console.log( c.toLocaleTimeString() );
@@ -67,10 +102,12 @@
    // set up auto checks
    //----------------------------------------
    function doOnLoad() {
-     setInterval( function() { checkForCookies(); }, 3 * 1000);
-     setInterval( function() { checkGrimoire(); }, 60 * 1000);
+     setInterval( function() { checkForCookies(); }, 3 * 1000);  // 3 sec
+     setInterval( function() { checkGrimoire(); }, 60 * 1000);  // 1 min  
+     setTimer( inactivityCheck, 30 * 60 * 1000 );   // 30 min
    };
 
+   // start the process
    doOnLoad();
 
 })();
