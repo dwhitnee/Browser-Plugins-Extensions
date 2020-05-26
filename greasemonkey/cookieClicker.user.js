7817873fbb8befa2e37a7dfe724d92289770ec32
David Whitney <dwhitnee@gmail.com>
2020-05-25 21:33:25 -0700

    Conjure

--- greasemonkey/cookieClicker.user.js
+++ greasemonkey/cookieClicker.user.js
@@ -12,20 +12,32 @@
      
      if (cookieWrapper && cookieWrapper.children.length) {
        console.log("COOKIE!");
-       cookieWrapper.style.height = "30em";
+       cookieWrapper.style.height = "30em";   // sorta cheating
        cookieWrapper.style.width = "30em";
        cookieWrapper.style.backgroundColor = "yellow";
+       cookieWrapper.children[0].click();   // super cheating
      } else {
        // restore?
-       console.log("nope...");
        cookieWrapper.style.height = "";
        cookieWrapper.style.width = "";
        cookieWrapper.style.backgroundColor = "";
      }
    }
 
+   function checkGrimoire() {
+     let t = document.getElementById("grimoireBarText");
+     let state = t.innerText.split("/");
+     if (state[0] == state[1]) {
+       // max charge, lets cast a spell!
+       console.log("CASTING SPELL!" + state[0] + " magic available");
+       let conjureBakedGoods = document.getElementById("grimoireSpell0");
+       // conjureBakedGoods.click();
+    }
+   }
+
    function doOnLoad() {
-     setInterval( function() { checkForCookies(); }, 3000);
+     setInterval( function() { checkForCookies(); }, 3 * 1000);
+     setInterval( function() { checkGrimoire(); }, 3 * 60 * 1000);
    };
 
    doOnLoad();
