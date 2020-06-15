7afb973f1daeb650f986fec48383e784e27a9361
David Whitney <dwhitnee@gmail.com>
2020-06-14 22:33:49 -0700

    Updates every second.

--- greasemonkey/swim.user.js
+++ greasemonkey/swim.user.js
@@ -8,9 +8,32 @@
 // @grant        none
 // ==/UserScript==
 
+//----------------------------------------------------------------------
+//  The SPCC swimming pool currently has a reservation system by time
+//  and lane, and it's very hard to tell which lanes are available
+//  since all lanes and all times are displayed with a "0/1" or "1/1
+//  available" message regardless of whether they are available.
+// 
+// This script filters out all the "0/1" lanes (ie, the unavailable lanes).
+// NOTE: It runs only on page load so if you click to a day day you'll have
+// to reload the page.
+//----------------------------------------------------------------------
+
+
 (function() {
    'use strict';
 
+   //----------------------------------------
+   // create the container we'll put results in and wait for page to load
+   //----------------------------------------
+   let container = createContainer();
+   let lastDate = "";
+   setInterval( checkForNewData, 1000 );
+
+
+
+   //----------------------------------------
+   // look through all lanes listed and find the few that are available
    //----------------------------------------
    function findTimes() {
      let allSlots = document.getElementsByClassName("card-availability");
@@ -19,32 +42,83 @@
        if (allSlots[i].innerText.includes("1/1")) {
          slots.push( allSlots[i] );
        } else {
+         // hide taken lanes
          allSlots[i].parentElement.parentElement.parentElement.parentElement.style.display="none";
        }
      }
-     display("Found " + slots.length + " free lanes today");
-     
-     for (i=0; i< slots.length; i++) {
-       display( slots[i].parentNode.parentNode.children[0].innerText );
+
+     if (slots.length) {
+       display("Found " + slots.length + " free lanes for " + lastDate);
+       for (i=0; i< Math.min( slots.length, 15); i++) {
+         display("* " + slots[i].parentNode.parentNode.children[0].innerText );
+       }
+       if (slots.length > 15) {
+         display("* more...");
+       }
+     } else {
+       display("Just a sec...");
      }
    }
    
    //----------------------------------------
    function display( text ) {
+     container.appendChild( createTextDiv( text )); 
+   }
+
+   //----------------------------------------
+   // create a text div
+   //----------------------------------------
+   function createTextDiv( text ) {
      var div = document.createElement("div");
      var content = document.createTextNode( text );
      div.appendChild( content );
-     div.style.fontWeight = "bold";
-     // div.style.fontSize = "1.2em";
-     
-     let header = document.getElementsByClassName("client-website-header")[0];
-     // header[0].appendChild( div );
-     let body = document.getElementsByClassName("client-schedule-body")[0];
-     body.insertBefore( div, header ); 
+     return div;
    }
 
+   //----------------------------------------
+   // create a container to hold lane listing
+   //----------------------------------------
+   function createContainer() {
+     let div = createTextDiv("Hello");
+     div.style.border = "green 5px solid";
+     div.style.padding = "0.5em";
+     div.style.fontWeight = "bold";
 
+     let pageLocation = 
+       document.getElementsByClassName("client-website-header")[0];
+     let body = document.getElementsByClassName("client-schedule-body")[0];
+     body.insertBefore( div, pageLocation ); 
 
-   // wait for page to render
-   setTimeout( findTimes, 3000 );
+     return div;
+   }
+
+   //----------------------------------------
+   // Guess what date is displayed based on current display
+   //----------------------------------------
+   function checkDate() {
+     let elems = document.getElementsByClassName("date");
+     if (elems.length) {
+       return elems[0].innerText;  // "Wed, 17 Jun 2020"
+     } else {
+       return "";
+     }
+   }
+
+   //----------------------------------------
+   // see what the date is, if it's new, go get lane times
+   //----------------------------------------
+   function checkForNewData() {
+     let date = checkDate();
+     console.log("Date is " + date );
+
+     if (date != lastDate) {
+       // wipe slate
+       while (container.childNodes.length) {
+         container.removeChild( container.childNodes[0]);
+       }
+       lastDate = date;
+       findTimes();
+     }
+   }
+
 })();
