7811951ceb9cbbd377ff0245c26218532bb2dd51
David Whitney <dwhitnee@gmail.com>
2022-03-19 19:20:04 -0700

    Checkpoint

--- s3PhotoBucket/login.js
+++ s3PhotoBucket/login.js
@@ -6,19 +6,34 @@
 // Adapted from https://github.com/amazon-archives/amazon-cognito-auth-js/blob/master/sample/index.html//
 //----------------------------------------------------------------------
 
+function setup() {
+  let s3Params = { Bucket: "dwhitnee-videos", region: "us-west-1" };
+
+  let identityPool = "us-east-1:2e17d11e-11d5-4810-8620-9acae707e140";
+  let userPoolUrl = "cognito-idp.us-east-1.amazonaws.com/us-east-1_d8ZhbRImF";
+
+  let cognitoAppConfig = {
+    ClientId: "55htjchp4ehcfa4h4usfpmdjlf",
+    UserPoolId: "us-east-1_d8ZhbRImF",
+    AppWebDomain: "dwhitnee-photos.auth.us-east-1.amazoncognito.com",
+    TokenScopesArray: ["openid","email"],
+    IdentityProvider: "COGNITO"
+  };
+}
 
 
-//let homeURL = "https://dwhitnee-pictures.s3.amazonaws.com/index.html";
-let homeURL = "http://localhost/~dwhitney/Browser-Plugins-Extensions/s3PhotoBucket/cog.html";
+let homeURL = window.location.href;
+// let homeURL = "https://dwhitnee-pictures.s3.amazonaws.com/cog.html";
+// let homeURL = "http://localhost/~dwhitney/Browser-Plugins-Extensions/s3PhotoBucket/cog.html";
 
-let authData = {
+let cognitoAppConfig = {
   ClientId: "55htjchp4ehcfa4h4usfpmdjlf",
   UserPoolId: "us-east-1_d8ZhbRImF",
   AppWebDomain: "dwhitnee-photos.auth.us-east-1.amazoncognito.com",
   TokenScopesArray: ["openid","email"],  // like ['openid','email','phone']...
   IdentityProvider: "COGNITO", // COGNITO, Facebook, SignInWithApple, Google,LoginWithAmazon
-  RedirectUriSignIn: homeURL,  // the url on sign authin
-  RedirectUriSignOut: homeURL, // the url on sign out
+  RedirectUriSignIn: homeURL,  // post login page
+  RedirectUriSignOut: homeURL, // post logout page
   AdvancedSecurityDataCollectionFlag : false
 };
 
@@ -54,15 +69,10 @@ function howdy( region, identityPool ) {
       });
     }
   });
-
-
-  // document.getElementById("signInButton").addEventListener(
-  //   "click",
-  //   function() {
-  //     userButton(auth);
-  //   });
 }
 
+
+/*
 // wtf?
 function addCredsToAWSConfig( userPool ) {
 
@@ -86,10 +96,8 @@ function addCredsToAWSConfig( userPool ) {
     });
 }
 }
+*/
 
-function goLogin( err ) {
-  alert("Error!" + err);
-}
 
 // Initialize a cognito auth object.
 // https://us-east-1.console.aws.amazon.com/cognito/users?region=us-east-1#/pool/us-east-1_d8ZhbRImF/clients
@@ -101,73 +109,97 @@ function goLogin( err ) {
 // Select Implicit grant to have user pool JSON web tokens (JWT) returned to you from Amazon Cognito. You can use this flow when there's no backend available to exchange an authorization code for tokens.
 
 
+function initAWS( region, UserPoolId ) {
+}
 
 //----------------------------------------------------------------------
+//  Create Cognito object that handles authentication and
 //----------------------------------------------------------------------
-function getAuthFromCognito() {
-  console.log("Creating auth object");
+async function doCognitoAuth( params ) {
+  console.log("Starting auth process");
 
-  // https://us-east-1.console.aws.amazon.com/cognito/users/?region=us-east-1#/pool/us-east-1_d8ZhbRImF/app-integration-app-settings
+  // requires old library
+  var auth = new AmazonCognitoIdentity.CognitoAuth( params.cognitoAppConfig );
 
-  // requires shitty library
-  var auth = new AmazonCognitoIdentity.CognitoAuth( authData );
-  auth.userhandler = {
-    onFailure: function goLogin( err ) { alert( err ); },
-    onSuccess: function getAuthToken( result ) {
-      console.log("Signed in!");
+  let authPromise = new Promise((resolve, reject) =>  {
+    auth.userhandler = {
+      //----------
+      // cognito logged us in, create AWS creds
+      //----------
+      onSuccess: function getAuthToken( result ) {
+        console.log("Signed in!");
+        let userPoolUrl = params.userPoolUrl;
+        AWS.config.region = params.authRegion;
+        AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+          IdentityPoolId: params.identityPoolId,
+          Logins: {
+            [userPoolUrl]: result.idToken.jwtToken   // link user with identity
+          }
+        });
 
-      // HOW TF TO GET AN AWS CREDS OBJECT!!!
-
-      /* Use the idToken for Logins Map when Federating User Pools with
-       identity pools or when passing through an Authorization Header to an
-       API Gateway Authorizer */
-      var idToken = result.idToken.jwtToken;
-
-      // Is this the magic?
-      // accessToken = result.getAccessToken().getJwtToken();
-
-      let userPoolUrl = "cognito-idp.us-east-1.amazonaws.com/us-east-1_d8ZhbRImF";
-
-      AWS.config.region = "us-east-1";
-      AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-        IdentityPoolId: "us-east-1:2e17d11e-11d5-4810-8620-9acae707e140",
-        Logins: {
-          [userPoolUrl]: result.idToken.jwtToken
-        }
-      });
-
-      // do stuff!
-      AWS.config.credentials.refresh( (error) => {
-        if (error) {
-          console.error( error );
-        } else {
-          s3 = new AWS.S3( {
-            apiVersion: '2006-03-01',
-            params: { Bucket: "dwhitnee-videos", region: "us-west-1" }
-          });
-          s3Bucket = "dwhitnee-videos";
-          displayPage();
-        }
-      });
-
-    }
-  };
-
+        resolve();
+      },
+      //----------
+      onFailure: function goLogin( err ) {
+        console.err( err );
+        alert( err );
+        reject( err );
+      }
+    };
+  });
 
   auth.parseCognitoWebResponse( window.location.href );
-
- //   auth.signOut();
   auth.getSession();
 
-  // The default response_type is "token",
-  // uncomment the next line will make it be "code".
+  // The default response_type is "token", next line will make it be "code".
   // auth.useCodeGrantFlow();
 
-  return auth;
+  // auth.signOut();
+
+  let result = await authPromise;
 }
 
 
 function login() {
   console.log("Checking auth");
-  let auth = getAuthFromCognito();
+
+  let cognitoAppConfig = {
+    ClientId: "55htjchp4ehcfa4h4usfpmdjlf",
+    UserPoolId: "us-east-1_d8ZhbRImF",
+    AppWebDomain: "dwhitnee-photos.auth.us-east-1.amazoncognito.com",
+    TokenScopesArray: ["openid","email"],
+    IdentityProvider: "COGNITO",
+    RedirectUriSignIn:  window.location.href,
+    RedirectUriSignOut: window.location.href
+  };
+
+  let authData = {
+    cognitoAppConfig: cognitoAppConfig,
+    authRegion: "us-east-1",
+    userPoolUrl: "cognito-idp.us-east-1.amazonaws.com/us-east-1_d8ZhbRImF",
+    IdentityPoolId: "us-east-1:2e17d11e-11d5-4810-8620-9acae707e140"
+  };
+
+  doCognitoAuth( authData ).then( function() {
+    doStuff();
+  });
+}
+
+
+function doStuff() {
+
+  AWS.config.credentials.refresh( (error) => {
+    if (error) {
+      console.error( error );
+    } else {
+      s3 = new AWS.S3( {
+        apiVersion: '2006-03-01',
+        params: { Bucket: "dwhitnee-videos", region: "us-west-1" }
+      });
+      s3Bucket = "dwhitnee-videos";
+      displayPage();
+
+      let hrefURL = s3.getSignedUrl('getObject', { Bucket: 'bucket', Key: 'key'});
+    }
+  });
 }
